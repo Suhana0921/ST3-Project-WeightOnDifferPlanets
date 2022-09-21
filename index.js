445694fa75db303ef383610a7a9c3899afbd6cea
Suhana <suhana0921.be20@chitkara.edu.in>
2022-09-21 19:13:51 +0530

    ST3

---
+++ index.js
@@ -0,0 +1,45 @@
+function earth(){
+    var weight = document.getElementById("name").value;
+    weight = weight*1;
+    document.getElementById("uc").innerHTML = "Your weight On earth is: " + weight+" Kg";
+}
+function jupiter(){
+    var weight = document.getElementById("name").value;
+    weight = weight*2.34;
+    document.getElementById("la").innerHTML = "Your weight On Jupiter is: " + weight+" Kg";
+}
+function mars(){
+    var weight = document.getElementById("name").value;
+    weight = weight * 0.38;
+    document.getElementById("ud").innerHTML = "Your weight On Mars  is: " + weight+" Kg";
+}
+function mercury(){
+    var weight = document.getElementById("name").value;
+    weight = weight * 0.37;
+    document.getElementById("ua").innerHTML = "Your weight On Mercury is: " + weight+" Kg";
+}
+function neptune(){
+    var weight = document.getElementById("name").value;
+    var neptune = weight * 1.19;
+    document.getElementById("ld").innerHTML = "Your weight On Neptune is: " + weight+" Kg";
+}
+function saturn(){
+    var weight = document.getElementById("name").value;
+    weight = weight * 1.06;
+    document.getElementById("lb").innerHTML = "Your weight On Saturn is: " + weight+" Kg";
+}
+function uranus(){
+    var weight = document.getElementById("name").value;
+    weight = weight * 0.92;
+    document.getElementById("lc").innerHTML = "Your weight On Uranus is: " + weight+" Kg";
+}
+function venus(){
+    var weight = document.getElementById("name").value;
+    weight = weight * 0.91;
+    document.getElementById("ub").innerHTML = "Your weight On Venus is: " + weight+" Kg";
+}
+function pluto(){
+    var weight = document.getElementById("name").value;
+    weight = weight * 0.06;
+    document.getElementById("le").innerHTML = "Your weight On Pluto is: " + weight+" Kg";
+}
